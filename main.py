a9c7f04edd68a14c7421e4904a29805fda2f07ed
HX-FNegrete <fran.negrete@hotmail.com>
2023-04-19 12:10:59 -0300

    cambios

--- main.py
+++ main.py
@@ -5,15 +5,16 @@ username = 'HX-FNegrete'
 token = input('Ingresa tu Token Personal de Seguridad: ')
 
 # Defimos la URL del repo 
-url = 'https://raw.githubusercontent.com/soyHenry/DS-M2-Checkpoint/blob/main/Generador-CP/CP_07_2.md'
+#url = 'https://raw.githubusercontent.com/soyHenry/DS-M2-Checkpoint/blob/main/Generador-CP/CP_07_2.md'
+url = 'https://raw.githubusercontent.com/HX-FNegrete/cp-automatico'
 
 # Enviamos la solicitud HTTP autenticada
 response = requests.get(url, auth=(username, token))
 
 # Si la solicitud fue exitosa, se lee el contenido del archivo Markdown
-if response.ok:
+if response.status_code == 200:
     markdown_content = response.text
     print(markdown_content)
 else:
-    print('No se pudo leer el archivo')
+    print(f"Error al descargar archivo: {response.status_code}")
 
